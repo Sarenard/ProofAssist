2ca5309cbe2adc4219a251bcd6b64eb74b458de8
Sarah <74245537+Sarenard@users.noreply.github.com>
2024-07-25 21:06:26 +0200

    some more safety

--- src/main.rs
+++ src/main.rs
@@ -22,11 +22,15 @@ fn main() {
     println!("{:?}", lambdaterme);
 
     while lambdaterme.clone().containsgoal() {
+        let mut bad = false;
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Failed to read line");
         input = input.trim().to_string();
         let splitted: Vec<&str> = input.split_whitespace().take(2).collect();
-        let array = [splitted[0], splitted[1]];
+        if splitted.len() < 2 {
+            bad = true;
+        }
+        let array = if !bad {[splitted[0], splitted[1]]} else {["_", "_"]};
         match array {
             ["intro", var] => {
                 lambdaterme = lambdaterme.intro(var.to_string());
@@ -37,8 +41,11 @@ fn main() {
                 println!("{:?}", lambdaterme);
             }
             _ => {
-                println!("Command not recognised !");
+                bad = true;
             }
+        }
+        if bad {
+            println!("Invalid command, please retry.");
         }
     }
 
